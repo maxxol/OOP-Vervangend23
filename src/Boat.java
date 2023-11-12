818c0c3ca88d981295aa706643be9a28135f1742
Jeroen Lüling <jtube2014@gmail.com>
2023-11-12 14:44:19 +0100

    improved constructor for Boat, added taxPercentage to Property

--- src/Boat.java
+++ src/Boat.java
@@ -2,5 +2,6 @@ public class Boat extends Property{
     public Boat(int givenValue,String givenOwner) {
         value = givenValue;
         owner = givenOwner;
+        taxPercentage = 0.0152; //not truly a percentage, 1.52/100 for value * percentage
     }
 }
--- src/Property.java
+++ src/Property.java
@@ -1,11 +1,13 @@
 public class Property {
     protected int value;
     protected String owner;
+    protected double taxPercentage;
 
     public int getValue(){
         return value;
     }
-    public String getOwner(){
-        return owner;
+    public String getOwner(){return owner;}
+    public double getTaxPercentage(){
+        return taxPercentage;
     }
 }
