21a78184603e735ecb5e3003a34daafcb482eac0
Jeroen Lüling <jtube2014@gmail.com>
2023-11-12 20:53:21 +0100

    added comments to new code, changed == to equals() in condition checking the owner to avoid null references

--- src/ObjectCreator.java
+++ src/ObjectCreator.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class ObjectCreator {
 
     public static void createObjects(){ //creates all the objects so main stays clean
@@ -15,33 +17,33 @@ public class ObjectCreator {
         House house3 = new House(300,"frank inc");
         House house4 = new House(400,"Bernard jr");
 
-        Property[] listOfProperties = {
+        Property[] listOfProperties = { //make a list with all the properties in it
               boat1, boat2, boat3, boat4,
               house1, house2, house3, house4
         };
 
-        String taxCountsBooleanMessage;
+        String taxCountsBooleanMessage; //initiating some variables for the loop
         double taxesOwed=0;
-        for (Property property:listOfProperties) {
-            if (property.getOwner()=="frank inc"){
+
+        for (Property property:listOfProperties) { //goes through every object in the list. "Property" because all object inherit from it and can thus be called by it
+            if (Objects.equals(property.getOwner(), "frank inc")){ //using .equals instead of == because it's null safe(I totally thought of that myself and not because intellij put a yellow line of depression under it)
             taxesOwed+=property.getValue()*property.getTaxPercentage();
-            taxCountsBooleanMessage = " added to total tax";
+            taxCountsBooleanMessage = " added to total tax"; //conditional message at the end of line
             }
-            else {taxCountsBooleanMessage=" NOT added to total tax";}
+            else {taxCountsBooleanMessage=" NOT added to total tax";} //if owner is not targeted owner
             System.out.println(
-                    property.getOwner()
+                    property.getOwner() //print the owner
                     +" owns a "+
-                    property.getObjectType()
+                    property.getObjectType() //print what type the object is
                     +", valued at $"+
-                    property.getValue()
+                    property.getValue() //print the value
                     +" with a tax percentage of "+
-                    String.format("%.2f",property.getTaxPercentage()*100)
+                    String.format("%.2f",property.getTaxPercentage()*100) //print the tax percentage as an actual percentage%, rounded to 2 decimals
                     +"%. tax = $"+
-                    String.format("%.2f",property.getValue()*property.getTaxPercentage())
-                    +taxCountsBooleanMessage
+                    String.format("%.2f",property.getValue()*property.getTaxPercentage())//print owed taxes over the object, rounded to 2 decimals
+                    +taxCountsBooleanMessage //add conditional message
             );
         }
-        System.out.println("total tax: $"+String.format("%.2f",taxesOwed));
-
+        System.out.println("total tax: $"+String.format("%.2f",taxesOwed)); //print total owed taxes to target company, rounded to 2 decimals
     }
 }
