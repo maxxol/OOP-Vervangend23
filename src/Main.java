423ea30295664b0aabcff05b471d9b9382aabca4
Jeroen Lüling <jtube2014@gmail.com>
2023-11-12 17:13:03 +0100

    removed placeholder constructor calls from main, made ObjectCreator loop through all boats and print properties

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main { //where everything comes together
     public static void main(String[] args) {
-        Boat boot1 = new Boat(10,"frank inc"); //placeholder constructor calls
-        Boat boot2 = new Boat(10,"frank inc");
-        Boat boot3 = new Boat(10,"frank inc");
-        Boat boot4 = new Boat(10,"frank inc");
-        System.out.println(boot1.getTaxPercentage()+" "+boot1.getValue()+" "+boot1.getOwner()); //placeholder object checker
+        ObjectCreator.createObjects();
     }
 }
--- src/ObjectCreator.java
+++ src/ObjectCreator.java
@@ -6,9 +6,9 @@ public class ObjectCreator {
         ArrayList<House> houses = new ArrayList<>();
         //boats
         Boat boot1 = new Boat(10,"frank inc"); //all of these are placeholder objects currently
-        Boat boot2 = new Boat(10,"frank inc");
-        Boat boot3 = new Boat(10,"frank inc");
-        Boat boot4 = new Boat(10,"frank inc");
+        Boat boot2 = new Boat(20,"frank inc");
+        Boat boot3 = new Boat(30,"frank inc");
+        Boat boot4 = new Boat(40,"vexigo inc");
         boats.add(boot1);
         boats.add(boot2);
         boats.add(boot3);
@@ -23,5 +23,9 @@ public class ObjectCreator {
         houses.add(house2);
         houses.add(house3);
         houses.add(house4);
+
+        for (Boat boat:boats) {
+            System.out.println(boat.getTaxPercentage()+" "+boat.getValue()+" "+boat.getOwner());
+        }
     }
 }
