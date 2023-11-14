9f562fc282fcd20e4413adb39c925032b198ee19
Jeroen Lüling <jtube2014@gmail.com>
2023-11-14 11:44:02 +0100

    added superconstructor to Property.java, calling superconstructor from House and Boat

--- src/House.java
+++ src/House.java
@@ -1,8 +1,5 @@
 public class House extends Property{ //class that creates House objects
     public House(int givenValue,String givenOwner) {
-        setObjectType("House");
-        setValue(givenValue); //set the values of the property to the assigned values
-        setOwner(givenOwner);
-        setTaxPercentage(0.0085); //not truly a percentage, 1.52/100 for value * percentage
+        super("House", givenValue, givenOwner, 0.0085);
     }
 }
