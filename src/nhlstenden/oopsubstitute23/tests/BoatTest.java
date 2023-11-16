0fef0b49c8d1e7ee8d110590530604ea64ab1da6
Jeroen Lüling <jtube2014@gmail.com>
2023-11-16 12:06:42 +0100

    added JUnit library, added and finished a test for the Boat object

---
+++ src/nhlstenden/oopsubstitute23/tests/BoatTest.java
@@ -0,0 +1,23 @@
+package nhlstenden.oopsubstitute23.tests;
+import nhlstenden.oopsubstitute23.objects.propertyclasses.propertysubclasses.Boat;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BoatTest {
+    @Test
+    public void testBoatConstructor() {
+        // Given
+        int givenValue = 123;
+        String givenOwner = "Arnold Schwarzenegger";
+
+        // When
+        Boat boat = new Boat(givenValue, givenOwner);
+
+        // Then
+        assertEquals("Boat", boat.getObjectType());
+        assertEquals(givenValue, boat.getValue());
+        assertEquals(givenOwner, boat.getOwner());
+        assertEquals(0.0152, boat.getTaxPercentage(), 0.0001); // Using delta for deviation due to doubles being imprecise
+    }
+}
+
