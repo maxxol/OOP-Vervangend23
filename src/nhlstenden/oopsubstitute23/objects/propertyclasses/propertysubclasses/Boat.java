71584d44c885c093c35ea876612216999eb98638
Jeroen Lüling <jtube2014@gmail.com>
2023-11-17 11:49:16 +0100

    added interface propertySubClasses to facilitate polymorphism

--- src/nhlstenden/oopsubstitute23/objects/propertyclasses/propertysubclasses/Boat.java
+++ src/nhlstenden/oopsubstitute23/objects/propertyclasses/propertysubclasses/Boat.java
@@ -2,8 +2,11 @@ package nhlstenden.oopsubstitute23.objects.propertyclasses.propertysubclasses;
 
 import nhlstenden.oopsubstitute23.objects.propertyclasses.Property;
 
-public class Boat extends Property { //class that creates Property.propertysubclasses.Boat nhlstenden.oopsubstitute23.objects
+public class Boat extends Property implements propertyInterface{ //class that creates Boat objects
     public Boat(int givenValue,String givenOwner) {
         super("Boat", givenValue, givenOwner, 0.0152);
+    }
+    public void whatAmI() { //unused method to show polymorphism
+        System.out.println("I am a Boat");
     }
 }
---
+++ src/nhlstenden/oopsubstitute23/objects/propertyclasses/propertysubclasses/propertyInterface.java
@@ -0,0 +1,6 @@
+package nhlstenden.oopsubstitute23.objects.propertyclasses.propertysubclasses;
+
+//this interface exists to show polymorphism
+public interface propertyInterface {
+    public void whatAmI();
+}
